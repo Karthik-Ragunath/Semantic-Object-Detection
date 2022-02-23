08450af786b34f0b187318562e2f948f7c64fd6f
Karthik-Ragunath <a.karthikragunath@gmail.com>
2022-02-22 22:49:36 -0600

    cropping images

--- inference.py
+++ inference.py
@@ -10,7 +10,7 @@ device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cp
 # load the model and the trained weights
 model = create_model(num_classes=5).to(device)
 model.load_state_dict(torch.load(
-    'outputs/model3.pth', map_location=device
+    'outputs/model10.pth', map_location=device
 ))
 model.eval()
 
